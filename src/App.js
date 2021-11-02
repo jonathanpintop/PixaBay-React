b1b38c57135796a083507a78b339e97cc9d1c34f
Jonathan Pinto <jonathanpintopp@gmail.com>
2021-11-02 10:18:32 -0500

    feat: Images List added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
+import ListadoImagenes from "./components/ListadoImagenes";
 
 function App() {
   // state de la app
   const [busqueda, guardarBusqueda] = useState("");
+  const [imagenes, guardarImagenes] = useState([]);
 
   useEffect(() => {
     const consultarApi = async () => {
@@ -16,7 +18,7 @@ function App() {
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
 
-      guardarBusqueda(resultado.hits);
+      guardarImagenes(resultado.hits);
     };
 
     consultarApi();
@@ -28,6 +30,10 @@ function App() {
         <p className="lead text-center">Buscador de imagenes</p>
 
         <Formulario guardarBusqueda={guardarBusqueda} />
+      </div>
+
+      <div className="row justify-content-center">
+        <ListadoImagenes imagenes={imagenes} />
       </div>
     </div>
   );
